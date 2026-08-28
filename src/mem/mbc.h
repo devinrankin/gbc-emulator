e96e86bf617faa56cd52506f1c63aba0f2fd19f6
Devin Rankin <devinrankin@proton.me>
2026-08-28 03:53:47 -0400

    mbc header: initial implementation

---
+++ src/mem/mbc.h
@@ -0,0 +1,47 @@
+#ifndef MBC_H
+#define MBC_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef enum mbc_type {
+    MBC_TYPE_NONE,
+    MBC_TYPE_MBC1,
+    MBC_TYPE_MBC2,
+    MBC_TYPE_MBC3,
+    MBC_TYPE_MBC5,
+    MBC_TYPE_MBC6,
+    MBC_TYPE_MBC7,
+    MBC_TYPE_MMM01,
+    MBC_TYPE_M161,
+    MBC_TYPE_HUC1,
+    MBC_TYPE_HUC3,
+    MBC_TYPE_OTHER
+} mbc_type_t;
+
+typedef struct mbc mbc_t;
+
+struct mbc {
+    mbc_type_t type;
+
+    uint8_t* rom;
+    uint8_t* ram;
+   
+    size_t rom_size;
+    size_t ram_size;
+    
+    bool ram_enabled;
+    uint16_t rom_bank;
+    uint8_t ram_bank;
+
+    uint8_t (*read)(mbc_t* mbc, uint16_t address);
+    void (*write)(mbc_t* mbc, uint16_t address, uint8_t value);
+
+};
+
+void mbc_init(mbc_t* mbc, mbc_type_t type, uint8_t* rom, size_t rom_size, uint8_t* ram, size_t ram_size);
+uint8_t mbc_read(mbc_t* mbc, uint16_t address);
+void mbc_write(mbc_t* mbc, uint16_t address, uint8_t value);
+
+#endif
