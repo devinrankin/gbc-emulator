89b49034b04c5e04df25e0c5fd197be51675f03c
Devin Rankin <devinrankin@proton.me>
2026-08-31 08:00:16 -0400

    bus header: add stdint include

--- src/mem/bus.h
+++ src/mem/bus.h
@@ -1,8 +1,15 @@
 #ifndef BUS_H
 #define BUS_H
 
-#include "../common.h"
+#include <stdint.h>
 #include "../mem/cartridge.h"
+
+typedef struct {
+    cartridge_t* cartridge;
+
+    uint8_t vram[0x2000];
+
+} bus_t;
 
 uint8_t bus_read8(cartridge_t* cartridge, uint16_t addr);
 void bus_write8(cartridge_t* cartridge, uint16_t addr, uint8_t value);
