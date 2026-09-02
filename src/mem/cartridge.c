da894a51fc41ade2a4ae060a6f0a3edf70390b8f
Devin Rankin <devinrankin@proton.me>
2026-09-02 12:27:06 -0400

    cartridge source: reformat static functions; use mbc read and write

--- src/mem/cartridge.c
+++ src/mem/cartridge.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+
 #include "cartridge.h"
+
+static uint8_t cartridge_title_len(cartridge_t* cartridge);
+static void cartridge_parse_title(cartridge_t* cartridge);
+static void cartridge_parse_header(cartridge_t* cartridge);
+static mbc_type_t cartridge_identify_mbc_type(uint8_t type);
+static bool cartridge_load_file(cartridge_t* cartridge, const char* path);
+
+void cartridge_init(cartridge_t* cartridge, const char* path) {
+    cartridge_load_file(cartridge, path);
+    cartridge_parse_header(cartridge);
+    
+    mbc_init(&cartridge->mbc, cartridge_identify_mbc_type(cartridge->header.cartridge_type), cartridge->rom, cartridge->rom_size, cartridge->ram, cartridge->ram_size);
+}
+
+void cartridge_display_info(cartridge_t* cartridge) {
+    printf("Displaying cartridge header information: \n");
+    printf("\tTitle       : %s\n", cartridge->header.title);
+    printf("\tMapper Type : %2.2x\n", cartridge->header.cartridge_type);
+    printf("\tROM Size    : %2.2x KiB\n", 32 << cartridge->header.rom_size);
+    printf("\tRAM Size    : %2.2x\n", cartridge->header.ram_size);
+}
+
+uint8_t cartridge_read8(cartridge_t* cartridge, uint16_t address) {
+   return cartridge->mbc.read(&cartridge->mbc, address); 
+}
+
+void cartridge_write8(cartridge_t* cartridge, uint16_t address, uint8_t value) {
+    cartridge->mbc.write(&cartridge->mbc, address, value);
+}
 
 static uint8_t cartridge_title_len(cartridge_t* cartridge) {
     uint8_t cgb_flag = cartridge->rom[CART_HDR_CGB_FLAG];
@@ -102,35 +132,4 @@ static bool cartridge_load_file(cartridge_t* cartridge, const char* path) {
     cartridge->rom_size = size;
     return true;
 }
-
-void cartridge_init(cartridge_t* cartridge, const char* path) {
-    cartridge_load_file(cartridge, path);
-    cartridge_parse_header(cartridge);
-    
-    mbc_init(&cartridge->mbc, cartridge_identify_mbc_type(cartridge->header.cartridge_type), cartridge->rom, cartridge->rom_size, cartridge->ram, cartridge->ram_size);
-}
-
-
-void cartridge_display_info(cartridge_t* cartridge) {
-    printf("Displaying cartridge header information: \n");
-    printf("\tTitle       : %s\n", cartridge->header.title);
-    printf("\tMapper Type : %2.2x\n", cartridge->header.cartridge_type);
-    printf("\tROM Size    : %2.2x KiB\n", 32 << cartridge->header.rom_size);
-    printf("\tRAM Size    : %2.2x\n", cartridge->header.ram_size);
-}
-
-uint8_t cartridge_read8(cartridge_t* cartridge, uint16_t address) {
-    if(address < 0x4000) {
-        return cartridge->rom[address];
-    }
-    NO_IMPL;
-}
-
-void cartridge_write8(cartridge_t* cartridge, uint16_t address, uint8_t value) {
-    if(address < 0x4000) {
-        cartridge->rom[address] = value;
-    }
-    NO_IMPL
-}
-
 
