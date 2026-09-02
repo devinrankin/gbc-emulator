d4a343681f4a92ca9544291f6cca84926f5e8a80
Devin Rankin <devinrankin@proton.me>
2026-09-02 14:41:36 -0400

    mbc header: change rom to readonly, remove bools

--- src/mem/mbc.h
+++ src/mem/mbc.h
@@ -2,7 +2,6 @@
 #define MBC_H
 
 #include <stdint.h>
-#include <stdbool.h>
 #include <stddef.h>
 
 typedef enum mbc_type {
@@ -25,22 +24,25 @@ typedef struct mbc mbc_t;
 struct mbc {
     mbc_type_t type;
 
-    uint8_t* rom;
+    const uint8_t* rom;
     uint8_t* ram;
    
     size_t rom_size;
     size_t ram_size;
     
-    bool ram_enabled;
+    uint8_t ram_enabled;
     uint16_t rom_bank;
     uint8_t ram_bank;
 
     uint8_t (*read)(mbc_t* mbc, uint16_t address);
     void (*write)(mbc_t* mbc, uint16_t address, uint8_t value);
 
+    uint8_t banking_mode;
+    uint8_t bank_low;
+    uint8_t bank_high;
 };
 
-void mbc_init(mbc_t* mbc, mbc_type_t type, uint8_t* rom, size_t rom_size, uint8_t* ram, size_t ram_size);
+void mbc_init(mbc_t* mbc, mbc_type_t type, const uint8_t* rom, size_t rom_size, uint8_t* ram, size_t ram_size);
 uint8_t mbc_read(mbc_t* mbc, uint16_t address);
 void mbc_write(mbc_t* mbc, uint16_t address, uint8_t value);
 
