3ec936c84b8ba46673b71fdf6d9ca0da7ef7bcae
Devin Rankin <devinrankin@proton.me>
2026-09-02 15:16:02 -0400

    gbc source: fix variables being local

--- src/gbc.c
+++ src/gbc.c
@@ -1,12 +1,8 @@
 #include "gbc.h"
 
 void gbc_init(gbc_t* gbc, const char* rom_path) {
-    cartridge_t cartridge;
-    sm83_t sm83;
-    bus_t bus;
-
-    cartridge_init(&cartridge, rom_path);
-    sm83_init(&sm83);
-    bus_init(&bus, &cartridge);
+    cartridge_init(&gbc->cartridge, rom_path);
+    sm83_init(&gbc->sm83);
+    bus_init(&gbc->bus, &gbc->cartridge);
 }
 
