f8fe1e54c155c59f3e388d6e58f1c909dcf3ebdf
Devin Rankin <devinrankin@proton.me>
2026-09-10 00:00:52 -0400

    main: local gbc

--- src/main.c
+++ src/main.c
@@ -2,9 +2,10 @@
 #include "gbc.h"
 
 
-static gbc_t gbc;
 
 int main(int argc, char* argv[]) {
+    gbc_t gbc;
+
     gbc_init(&gbc, argv[1]);
     cartridge_display_info(&gbc.cartridge);
     return 0;
